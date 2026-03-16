50d795f21675c54bda0010e25d0cf37ac37b9040
xu0613 <2053961265@qq.com>
2026-03-16 20:06:33 +0800

    Scanner + If

--- java-basics/Main.java
+++ java-basics/Main.java
@@ -1,7 +1,90 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
-        System.out.println("My name is Jeremy.");
-        System.out.println("Today is my first day learning Java.");
+//        System.out.println("Hello, Java!");
+
+
+//
+//        int age = 18;
+//        double height = 192;
+//        char grade = 'A';
+//        boolean isStudent = true;
+//        String major = "ICS";
+//
+//        System.out.println("name: " + name);
+//        System.out.println("Age: " + age);
+//        System.out.println("Height: " + height);
+//        System.out.println("grade: " + grade);
+//        System.out.println("isStudent: " + isStudent);
+//        System.out.println("major: " + major);
+
+
+//        String name = "Jeremy";
+//        int age = 18;
+//        double height = 192.5;
+//
+//        double a = 10;
+//        double b = 3;
+//
+//        System.out.println("Name: " + name);
+//        System.out.println("Age: " + age);
+//        System.out.println("Height: " + height);
+//
+//        System.out.println("a + b = " + (a + b));
+//        System.out.println("a - b = " + (a - b));
+//        System.out.println("a * b = " + (a * b));
+//        System.out.println("a / b = " + (a / b));
+//        System.out.println("a % b = " + (a % b));
+
+
+        Scanner input = new Scanner(System.in);
+
+//        System.out.print("Enter your age: ");
+//        int age = input.nextInt();
+//        input.nextLine();
+//
+//        System.out.print("Enter your name: ");
+//        String name = input.nextLine();
+//
+//        System.out.println("Name: " + name);
+//        System.out.println("Age: " + age);
+
+//        System.out.print("Enter your score: ");
+//        int score = input.nextInt();
+////        double score = input.nextDouble();
+//
+//        if (score >= 90) {
+//            System.out.println("A");
+//        } else if (score >= 80) {
+//            System.out.println("B");
+//        } else if (score >= 70) {
+//            System.out.println("C");
+//        } else if (score >= 60) {
+//            System.out.println("D");
+//        } else {
+//            System.out.println("F");
+//        }
+
+
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter your score: ");
+        int score = input.nextInt();
+
+        if (score >= 90) {
+            System.out.println(name + " passed the exam, you got A");
+            System.out.println("Excellent Work");
+        } else if (score >= 80) {
+            System.out.println(name + " passed the exam, you got B");
+        } else if (score >= 70) {
+            System.out.println(name + " passed the exam, you got C");
+        } else if (score >= 60) {
+            System.out.println(name + " passed the exam, you got D");
+        } else {
+            System.out.println(name + " failed the exam");
+        }
+
     }
 }
