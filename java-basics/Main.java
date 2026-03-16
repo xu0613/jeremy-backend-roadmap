0bba029ead8d053a7bdf2fef40b778c356d14258
xu0613 <2053961265@qq.com>
2026-03-16 13:59:47 +0800

    set up Java environment and run first program

---
+++ java-basics/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        System.out.println("My name is Jeremy.");
+        System.out.println("Today is my first day learning Java.");
+    }
+}
--- java-basics/README.md
+++ java-basics/README.md
@@ -1,1 +1,6 @@
 # Java Basics
+## Day 1
+- Installed JDK 21
+- Set up IntelliJ IDEA
+- Created first Java program
+- Learned `public class Main` and `System.out.println()`
