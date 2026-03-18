1c8b8c30c385cdcdf41034017be3fca723efbe33
xu0613 <2053961265@qq.com>
2026-03-18 13:17:12 +0800

    Day 2 Exercise

---
+++ java-basics/exercise/OddEven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Eneter a number: ");
+        int number = input.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+    }
+}
